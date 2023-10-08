6f480d9b6cf2f79c176d83136ad0d53c07a9d833
Sam Machin <sam@sammachin.com>
2023-10-08 16:07:30 +0100

    update states

--- dimmablelight.js
+++ dimmablelight.js
@@ -5,31 +5,46 @@ module.exports = function(RED) {
         var node = this;
         node.bridge = RED.nodes.getNode(config.bridge);
         node.name = config.name
+        node.range = config.range
         console.log(`Loading Device node ${node.id}`)
+        console.log(`Range set to  ${node.range}`)
         node.status({fill:"red",shape:"ring",text:"not running"});
         this.on('input', function(msg) {
-            if (typeof msg.payload == "boolean") {
-                node.device.onOff(msg.payload)
-            } else if (typeof(msg.payload) == 'number'){
-                node.device.setCurrentLevel(msg.payload)
-            } else {
-                switch (msg.payload){
-                    case '1':
-                        node.device.onOff(true)
-                        break
-                    case '0':
-                        node.device.onOff(false)
-                        break
-                    case 'on':
-                        node.device.onOff(true)
-                        break
-                    case 'off':
-                        node.device.onOff(false)
-                        break
-                    case 'toggle':
-                        node.device.toggle()
-                        break
-                }
+            console.log(`Range set to  ${node.range}`)
+            if (msg.payload.state == undefined) {
+                console.log('Setting to current state')
+                msg.payload.state = node.device.getOnOff()
+            }
+            if (msg.payload.level == undefined) {
+                console.log('Setting to current level')
+                msg.payload.level = node.device.getCurrentLevel()
+            }
+            else {
+                if (node.range == "100"){ msg.payload.level = Math.round(msg.payload.level*2.54)}
+            }
+            node.device.setCurrentLevel(msg.payload.level)
+            switch (msg.payload.state){
+                case '1':
+                    node.device.setOnOff(true)
+                    break
+                case '0':
+                    node.device.setOnOff(false)
+                    break
+                case 'on':
+                    node.device.setOnOff(true)
+                    break
+                case 'off':
+                    node.device.setOnOff(false)
+                    break
+                case 'toggle':
+                    node.device.toggle()
+                    break
+                case false:
+                    node.device.setOnOff(false)
+                    break
+                case true:
+                    node.device.setOnOff(true)
+                    break
             }
         });
         this.on('serverReady', function() {
@@ -37,8 +52,10 @@ module.exports = function(RED) {
         })
         this.on('state', function(data){
             console.log(node.id, data)
-            var msg = {};
-            msg.payload=data
+            var msg = {payload : {}};
+            msg.payload.state = node.device.getOnOff()
+            msg.payload.level = node.device.getCurrentLevel()
+            if (node.range == "100"){ msg.payload.level = Math.round(msg.payload.level/2.54)}
             node.send(msg);
         })
         this.on('close', function(removed, done) {

424c8251aaf7a85b1c2db8f91a7a695d6b39edb1
Slyke <6442613+Slyke@users.noreply.github.com>
2026-09-18 01:47:13 -0700

    Fix color-light temperature bounds for Matter.js 0.17

--- devices/colortemplight.js
+++ devices/colortemplight.js
@@ -34,6 +34,8 @@ module.exports = {
                     reachable: true,
                 },
                 colorControl: {
+                    colorTempPhysicalMinMireds: 1,
+                    colorTempPhysicalMaxMireds: 0xFEFF,
                     coupleColorTempToLevelMinMireds: 0x00FA,
                     startUpColorTemperatureMireds: 0x00FA,
                     colorMode: 2
